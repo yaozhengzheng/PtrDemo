f2b2c6b6af099dfd5ca7dfbac4599dd63b0f9622
yao <1624566366@qq>
2016-08-19 15:15:07 +0800

    第一次修改，没什么

--- app/src/main/java/com/yao/feicui/ptrdemo/PtrPageView.java
+++ app/src/main/java/com/yao/feicui/ptrdemo/PtrPageView.java
@@ -5,6 +5,8 @@ import com.hannesdorfmann.mosby.mvp.MvpView;
 import java.util.List;
 
 /**
+ *
+ * hah
  * Created by 16245 on 2016/07/04.
  */
 public interface PtrPageView extends MvpView, PtrView<List<String>>,LoadMoreView<List<String>>{
